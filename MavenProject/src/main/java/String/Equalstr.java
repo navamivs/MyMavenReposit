a861e7a00124306d4f6605298e552808d88a895c
Navami <navami.vimaldas.suprabha@gmail.com>
2024-05-18 18:26:57 +0100

    yes

---
+++ MavenProject/src/main/java/String/Equalstr.java
@@ -0,0 +1,20 @@
+package String;
+
+public class Equalstr {
+
+	public static void main(String[] args) {
+		
+		String s1="Java";
+		String s2="Java";
+		String s3="java";
+		String s4="Python";
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));
+		System.out.println(s1.equals(s4));
+		System.out.println(s2.equals(s3));
+		System.out.println(s2.equals(s4));
+		
+	}
+
+}
